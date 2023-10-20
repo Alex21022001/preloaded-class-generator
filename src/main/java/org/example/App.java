97cedf2a9b1b4c8b6da713257210d360020cc375
Alexey <alexeysitiy@gmail.com>
2023-10-20 19:57:15 +0300

    init

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,11 +7,10 @@ public class App {
 
     public static void main(String[] args) {
         List<String> groupNames = List.of("/aws/lambda/tg-handler", "/aws/lambda/ws-handler", "/aws/lambda/lifecycle-handler");
-        Path output = Path.of("preloaded-classes.txt");
+        Path output = Path.of("quarkus-preload-classes.txt");
         Integer limit = 3;
 
         LogService logService = new LogService(groupNames, output, limit);
         logService.generateClasses();
     }
-
 }
