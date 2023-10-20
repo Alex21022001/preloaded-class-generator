2735d724c2715c645e6540c0ca86c68d0de9262a
Alexey <alexeysitiy@gmail.com>
2023-10-20 19:51:55 +0300

    init

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) {
+        List<String> groupNames = List.of("/aws/lambda/tg-handler", "/aws/lambda/ws-handler", "/aws/lambda/lifecycle-handler");
+        Path output = Path.of("preloaded-classes.txt");
+        Integer limit = 3;
+
+        LogService logService = new LogService(groupNames, output, limit);
+        logService.generateClasses();
+    }
+
+}
